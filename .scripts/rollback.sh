43e581ebf887ce9214be9dd5591dda3aec46dfd2
Siracgul <sirac.gul@hotmail.com>
2025-05-25 00:39:00 +0100

    Fix deploy flow

--- .scripts/rollback.sh
+++ .scripts/rollback.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Variables
 REV_PROXY_IP="13.61.19.221"
-FALLBACK_IP="13.60.180.198"  # Blue Environment IP
+PREV_TARGET_IP="13.60.64.217"  # GREEN değil de BLUE IP'si
 
-# SSH bağlantısı ile reverse proxy üzerindeki nginx config dosyasını geri döndür
 ssh -o StrictHostKeyChecking=no -i bl-gr-key.pem ubuntu@$REV_PROXY_IP << EOF
 sudo bash -c 'cat > /etc/nginx/sites-available/default << NGINXCONF
 server {
     listen 80;
     location / {
-        proxy_pass http://$FALLBACK_IP;
+        proxy_pass http://$PREV_TARGET_IP;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
     }
 }
 NGINXCONF
+
 sudo systemctl restart nginx
 '
 EOF
