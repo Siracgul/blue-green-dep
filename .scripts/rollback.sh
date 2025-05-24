b1e3a1f262e3139b2432f79e482a177cdeb14874
Siracgul <sirac.gul@hotmail.com>
2025-05-24 23:49:51 +0100

    Test

--- .scripts/rollback.sh
+++ .scripts/rollback.sh
@@ -2,9 +2,9 @@
 
 # Variables
 REV_PROXY_IP="13.61.19.221"
-FALLBACK_IP="13.60.180.198"  # Blue sunucu IP
+FALLBACK_IP="13.60.180.198"  # Blue Environment IP
 
-# SSH ile reverse proxy üzerindeki nginx config dosyasını geri döndür
+# SSH bağlantısı ile reverse proxy üzerindeki nginx config dosyasını geri döndür
 ssh -o StrictHostKeyChecking=no -i bl-gr-key.pem ubuntu@$REV_PROXY_IP << EOF
 sudo bash -c 'cat > /etc/nginx/sites-available/default << NGINXCONF
 server {
